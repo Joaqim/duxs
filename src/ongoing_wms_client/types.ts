4380588f247dcade6ea5caa96a5d0b4ad69073c5
Joaqim Planstedt <git@joaqim.xyz>
2024-05-13 21:28:42 +0200

    Add: const string array of 'get' endpoints

--- src/ongoing_wms_client/types.ts
+++ src/ongoing_wms_client/types.ts
@@ -16,6 +16,22 @@ export type GetEndpoint = KeyOfType<
   }
 >;
 
+export const AllGetEndpoints: Array<GetEndpoint> = [
+  "/api/v1/articleItems/statuses",
+  "/api/v1/articles/classes",
+  "/api/v1/articles/dangerousGoods",
+  "/api/v1/articles/historicalInventory",
+  "/api/v1/articles/inventoryPerWarehouse",
+  "/api/v1/articles/structureDefinitions",
+  "/api/v1/orders",
+  "/api/v1/orders/classes",
+  "/api/v1/orders/types",
+  "/api/v1/orders/wayOfDeliveryTypes",
+  "/api/v1/purchaseOrders/types",
+  "/api/v1/returnOrders",
+  "/api/v1/returnOrders/returnCauses",
+] as const;
+
 export type GetParameters<TEndpoint extends GetEndpoint> =
   paths[TEndpoint]["get"]["parameters"]["query"];
 export type GetModel<TEndpoint extends GetEndpoint> =
