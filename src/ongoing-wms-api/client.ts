e020a3e47aec206d5a76fe4f69501db65fd7d8d9
Joaqim Planstedt <mail@joaqim.xyz>
2026-08-28 17:56:27 +0200

    fix(client): actually use transport shim by default
    Signed-off-by: Joaqim Planstedt <mail@joaqim.xyz>

--- src/ongoing-wms-api/client.ts
+++ src/ongoing-wms-api/client.ts
@@ -1,5 +1,6 @@
 import type { paths } from "./gen/client.d.ts";
 import createClient from "openapi-fetch";
+import { transport } from "./transport.js";
 import { ClientWrapper } from "./utils";
 import { ArticlesApiV1 } from "./articles";
 import { OrdersApiV1 } from "./orders";
@@ -7,8 +8,8 @@ import { OrdersApiV1 } from "./orders";
 export class OngoingWMSClient extends ClientWrapper<paths> {
   public articlesApiV1: ArticlesApiV1;
   public ordersApiV1: OrdersApiV1;
-  constructor(options: { BASE_URL: string; TOKEN: string }) {
-    super(createClient<paths>({ baseUrl: options.BASE_URL }), options.TOKEN);
+  constructor(options: { BASE_URL: string; TOKEN: string; }, fetch = transport) {
+    super(createClient<paths>({ baseUrl: options.BASE_URL, fetch }), options.TOKEN);
     this.articlesApiV1 = new ArticlesApiV1(this.client);
     this.ordersApiV1 = new OrdersApiV1(this.client);
   }
