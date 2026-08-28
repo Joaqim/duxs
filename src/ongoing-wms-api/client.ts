cdac1fc8c5a9b31b4bad1a54c7a0eb24f8a28904
Joaqim Planstedt <mail@joaqim.xyz>
2026-08-28 17:58:04 +0200

    chore(fmt): format client
    Signed-off-by: Joaqim Planstedt <mail@joaqim.xyz>

--- src/ongoing-wms-api/client.ts
+++ src/ongoing-wms-api/client.ts
@@ -8,8 +8,11 @@ import { OrdersApiV1 } from "./orders";
 export class OngoingWMSClient extends ClientWrapper<paths> {
   public articlesApiV1: ArticlesApiV1;
   public ordersApiV1: OrdersApiV1;
-  constructor(options: { BASE_URL: string; TOKEN: string; }, fetch = transport) {
-    super(createClient<paths>({ baseUrl: options.BASE_URL, fetch }), options.TOKEN);
+  constructor(options: { BASE_URL: string; TOKEN: string }, fetch = transport) {
+    super(
+      createClient<paths>({ baseUrl: options.BASE_URL, fetch }),
+      options.TOKEN,
+    );
     this.articlesApiV1 = new ArticlesApiV1(this.client);
     this.ordersApiV1 = new OrdersApiV1(this.client);
   }
