7423ac4953053bb9a9a100d7f5b8d3d6e6e68049
Joaqim Planstedt <mail@joaqim.xyz>
2026-08-28 13:05:44 +0200

    chore: orders: remove duplicate types
    Signed-off-by: Joaqim Planstedt <mail@joaqim.xyz>

--- src/ongoing-wms-api/orders.ts
+++ src/ongoing-wms-api/orders.ts
@@ -1,31 +1,23 @@
-import type { components, operations, paths } from "./gen/orders.d.ts";
+import type { paths } from "./gen/orders.d.ts";
+import {
+  PostParcelUsingIdTypeModel,
+  PostParcelTypeModel,
+  PostOrderTrackingModel,
+  PostParcelTrackingModel,
+  PostOrderModel,
+  OrdersGetAllParamsQuery,
+  PostWayBillRowModel,
+  PostFileMode,
+  PatchOrderNumberModel,
+  PatchOrderTransporterModel,
+  PatchOrderReturnWaybill,
+  PatchServicePointCode,
+  PatchOrderWaybill,
+  PatchOrderDeliveryDate,
+  PatchFreightPrice,
+} from "./types/orders.types.js";
 import type { PostFileNoFilenameModel } from "./types/shared.types.ts";
 import { ClientWrapper } from "./utils";
-
-export type PostParcelUsingIdTypeModel =
-  components["schemas"]["PostParcelUsingIdTypeModel"];
-export type PostParcelTypeModel = components["schemas"]["PostParcelTypeModel"];
-export type PostOrderTrackingModel =
-  components["schemas"]["PostOrderTrackingModel"];
-export type PostParcelTrackingModel =
-  components["schemas"]["PostParcelTrackingModel"];
-export type PostOrderModel = components["schemas"]["PostOrderModel"];
-export type PostWayBillRowModel = components["schemas"]["PostWayBillRowModel"];
-export type PostFileMode = components["schemas"]["PostFileModel"];
-export type PatchOrderNumberModel =
-  components["schemas"]["PatchOrderNumberModel"];
-export type PatchOrderTransporterModel =
-  components["schemas"]["PatchOrderTransporterModel"];
-export type PatchOrderReturnWaybill =
-  components["schemas"]["PatchOrderReturnWaybill"];
-export type PatchServicePointCode =
-  components["schemas"]["PatchServicePointCode"];
-export type PatchOrderWaybill = components["schemas"]["PatchOrderWaybill"];
-export type PatchOrderDeliveryDate =
-  components["schemas"]["PatchOrderDeliveryDate"] | null;
-export type PatchFreightPrice = components["schemas"]["PatchFreightPrice"];
-export type OrdersGetAllParamsQuery =
-  operations["Orders_GetAll"]["parameters"]["query"];
 
 export class OrdersApiV1 extends ClientWrapper<paths> {
   updateParcelUsingId(
