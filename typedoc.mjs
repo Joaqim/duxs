09f845df99713b8a8fb7a2fd78ed61056d441f76
Joaqim Planstedt <mail@joaqim.xyz>
2026-08-29 13:35:45 +0200

    feat(typedoc): wip: init config
    Signed-off-by: Joaqim Planstedt <mail@joaqim.xyz>
    
    chore(fmt): typedoc conf
    
    Signed-off-by: Joaqim Planstedt <mail@joaqim.xyz>

---
+++ typedoc.mjs
@@ -0,0 +1,24 @@
+import { OptionDefaults } from "typedoc";
+
+/** @type {Partial<import('typedoc').TypeDocOptions>} */
+const config = {
+  ...OptionDefaults,
+  entryPoints: ["src/index.ts", "src/ongoing-wms-api/gen/**/*.types.ts"],
+  //entryPointStrategy: "expand",
+  //sort: ["source-order"],
+  cleanOutputDir: true,
+  excludePrivate: true,
+  excludeExternals: true,
+  externalPattern: [
+    "**/node_modules/**",
+    //"./src/ongoing-wms-api/gen/**"
+    "./src/ongoing-wms-api/gen/**/*.d.ts",
+  ],
+  validation: {
+    notExported: false,
+    invalidLink: true,
+    notDocumented: false,
+  },
+};
+
+export default config;
