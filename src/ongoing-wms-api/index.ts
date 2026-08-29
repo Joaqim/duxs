0189edc5677f8b5b6317087aa6d3bda8b9455d36
Joaqim Planstedt <mail@joaqim.xyz>
2026-08-29 15:02:24 +0200

    fix: export types from barrel file
    Signed-off-by: Joaqim Planstedt <mail@joaqim.xyz>

--- src/ongoing-wms-api/index.ts
+++ src/ongoing-wms-api/index.ts
@@ -2,3 +2,6 @@ export * from "./articles";
 export * from "./client";
 export * from "./orders";
 export type { FetchLike } from "./transport";
+export type * from "./types";
+export type * from "./gen/articles.types";
+export type * from "./gen/orders.types";
